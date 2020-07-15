89c24356cb00be444cbd9e6c0336182794e94683
Kareem Salah <37020590+Kymo97@users.noreply.github.com>
2020-07-15 05:45:05 +0200

    Lab w4d2

---
+++ w4d2/pets/ajaxpets.js
@@ -0,0 +1,28 @@
+window.onload=()=>{
+    $("#puppies").click(updatePic);
+    $("#kitties").click(updatePic);
+};
+let url ="http://mumstudents.org/cs472/2013-09/Sections/8/ajaxpets/ajaxpets.php";
+
+function updatePic(e){
+    //ajax solution
+        // $.ajax(url,{
+    //     "data":{"animal":this.value},
+    // }).done(displayPic)
+    // .fail((err)=>{console.log(err);});
+    fetch(url+"?animal="+this.value,{
+        method:'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(res=> res.text())
+    .then(displayPic)
+    .catch((err)=>console.log(err));
+}
+function displayPic(response){
+    console.log(response);
+    $("#pictures").children().remove();
+    $("#pictures").append(response);
+}
+
